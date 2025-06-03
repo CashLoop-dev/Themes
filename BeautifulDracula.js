180ef03fa27d58050254ac19e1327dfa154fa742
CashLoop-dev <ednut157@gmail.com>
2025-06-03 19:13:06 +0100

    Create BeautifulDracula.js

---
+++ BeautifulDracula.js
@@ -0,0 +1,23 @@
+t.prefs_.set('color-palette-overrides', [
+  "#000000", // black
+  "#ff5555", // red
+  "#50fa7b", // green
+  "#f1fa8c", // yellow
+  "#bd93f9", // blue (actually purple in Dracula)
+  "#ff79c6", // magenta
+  "#8be9fd", // cyan
+  "#bbbbbb", // white (light gray)
+
+  "#555555", // bright black
+  "#ff5555", // bright red
+  "#50fa7b", // bright green
+  "#f1fa8c", // bright yellow
+  "#bd93f9", // bright blue (purple)
+  "#ff79c6", // bright magenta
+  "#8be9fd", // bright cyan
+  "#ffffff"  // bright white
+]);
+
+t.prefs_.set('foreground-color', "#f8f8f2");           // Dracula foreground
+t.prefs_.set('background-color', "#282a36");           // Dracula background
+t.prefs_.set('cursor-color', 'rgba(248,248,242,0.5)'); // Dracula cursor (semi-transparent)
